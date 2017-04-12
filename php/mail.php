4c5f002b17e01c98e1aaf0081dcc9a4d42259dbd
spendlively <spendlively@mail.ru>
2017-04-12 09:57:26 +0700

    Исправил кодировку в mail.php

--- php/mail.php
+++ php/mail.php
@@ -19,7 +19,7 @@ $to .= "Spendlively <spendlively@yandex.ru>";
 $subject = "Subject";
 $message = '<html><head><title>Test</title></head><body><p>Test</p></body></html>';
 $headers= "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+$headers .= "Content-type: text/html; charset=utf-8\r\n";
 $headers .= "From: Myself <spendlively@gmail.com>\r\n";
 $headers .= "Cc: birthdayarchive@example.com\r\n";
 $headers .= "Bcc: birthdaycheck@example.com\r\n";
