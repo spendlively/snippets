b4505f500712bc4cb785e1ec9d5a41c4b40e8992
spendlively <spendlively@mail.ru>
2017-04-20 12:05:51 +0700

    Подправил jsonp

--- js/jsonp.js
+++ js/jsonp.js
@@ -6,9 +6,9 @@
 
 //Отправка запроса
 function addScript(src) {
-  var elem = document.createElement("script");
-  elem.src = src;
-  document.head.appendChild(elem);
+	var el = document.createElement("script");
+	el.src = src;
+	document.head.appendChild(elem);
 }
 
 //Обработка ответа
