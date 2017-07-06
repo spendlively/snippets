872fead44aae9893dd3df0e8a4c3c7743786bdd1
spendlively <spendlively@mail.ru>
2017-07-06 20:01:44 +0700

    События

--- frameworks/jquery/events.js
+++ frameworks/jquery/events.js
@@ -1,34 +1,35 @@
 
-// Готово DOM дерево
+//Готово DOM дерево
 $(document).ready(function () {
-    alert("Document loaded");
+    console.log('ready');
 });
 
-// load() - устанавливает обработчик на JavaScript событие load
-// window.load - Загружена вся страница с учетом всех внешних ресурсов (картинок, сценариев, стилей)
+
+
+//Загружена вся страница с учетом всех внешних ресурсов (картинок, сценариев, стилей)
 $(window).load(function () {
-    alert("Window loaded");
+    console.log('load');
 });
 
-//добавляет обработчик на JavaScript событие "click"
+
+
+//Cобытие "click"
 $("p").click(function(){
-    alert("clicked");
+    console.log('click');
 });
 
-//focus - событие происходит, когда элемент получает фокус
+
+
+//Событие focus/blur
 $("input").focus(function() {
-    $(this).val("focus");
+    console.log('focus');
 });
 
-//blur - событие происходит, когда элемент теряет фокус
-$("input").blur(function() {
-    $(this).val("blur");
-});
 
-// keydown() - устанавливает обработчик события keydown для каждого элемента набора.
-// event - объект, определяющий информацию о событии.
+
+//Событие keydown/keyup
 $(window).keydown(function (event) {
-    switch (event.keyCode) {// event.keyCode - код нажатой клавиши
+    switch (event.keyCode) {
         case 37: {
             alert("left pushed");
         }
@@ -48,54 +49,43 @@ $(window).keydown(function (event) {
     }
 });
 
-// keyup() - создает обработчик на событие keyup для каждого элемента набора.
-$(window).keyup(function (event) {
-    switch (event.keyCode) {
-        case 37: {
-            alert("left poped");
-        }
-            break;
-        case 38: {
-            alert("up poped");
-        }
-            break;
-        case 39: {
-            alert("right poped");
-        }
-            break;
-        case 40: {
-            alert("down poped");
-        }
-            break;
-    }
-});
 
-// keypress - событие, которое возникает при нажатии символьной клавиши клавиатуры
-// (не срабатывает для функциональных клавиш (Shift, Ctrl, Alt и т.д.)
+
+//Событие keypress (символьная клавиша клавиатуры)
+//(не срабатывает для функциональных клавиш (Shift, Ctrl, Alt и т.д.)
 $(window).keypress(function (event) {
-    alert(String.fromCharCode(event.charCode) + "(" + event.charCode + ")");
+    console.log(event.charCode);
+    console.log(String.fromCharCode(event.charCode));
 });
 
-// mouseover(f) - функция, которая принимает и назначает функцию "f" как обработчик на событие mouseover для каждого элемента набора.
-// событие происходит при входе курсора в область элемента.
-$("div:first").mouseover(function () { overCount++; Update(); }) // курсор попадает в область элемента
 
-// mouseout(f) - функция, которая принимает и назначает функцию "f" как обработчик на событие mouseout для каждого элемента набора.
-// событие происходит при выходе курсора в область элемента.
-$("div:last").mouseout(function () { outCount++; Update(); }) // курсор выходит за область элемента
 
-// hover(in, out) - функция, которая отвечает за привязку обработчиков на события mouseover и mouseout.
-$("div").hover(function(){}, function(){});
+//Событие mouseover/mouseout
+$("div").mouseover(function(){
+    console.log('mouseover');
+});
 
-//mousemove() - функция, устанавливающая обработчик на событие движения курсора по поверхности элемента.
+
+
+//Событие hover(in, out) - привязка обработчиков mouseover и mouseout
+$("div").hover(function(){
+    console.log('mouseover');
+}, function(){
+    console.log('mouseout');
+});
+
+
+
+//Событие mousemove() - движение курсора по поверхности элемента
 $("div").mousemove(function (e) {
-    e.pageX;
-    e.pageY;
+    console.log(e.pageX, e.pageY);
 });
 
-//Метод on(событие, обработчик) используется для привязки обработчиков на события к объекту или объектам.
-//принимает в качестве аругментов событие и функцию обработчик
-//Предусмотрено добавление дополнительных параметров. В отличии от addEventListener(), который может работать как на "перехвате"
+
+
+//Метод on(событие, обработчик) - привязка обработчиков на события к объекту или объектам.
+//Предусмотрено добавление дополнительных параметров.
+//В отличии от addEventListener(), который может работать как на "перехвате"
 $("ul").on("click", function (event) {
     $(event.target).css("background", "red");
 });
