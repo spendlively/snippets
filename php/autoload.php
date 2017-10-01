913b19309e53e61c97b42f7845ebb81a8d2204a3
spendlively <spendlively@mail.ru>
2017-10-01 16:06:38 +0700

    Добавил psr-0 разделенный подчеркиванием

--- php/autoload.php
+++ php/autoload.php
@@ -1,52 +1,66 @@
 <?php
 
-///////////////////////////////////////////////////////////////////////
-//////////////////////////////////PSR-4////////////////////////////////
-///////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+///////////////////////Автозагрузчик в composer.json//////////////////////
+//////////////////////////////////////////////////////////////////////////
 //1. composer.json
 '
 {
-    "autoload": {
-        "psr-4": {
-            "MyLibrary\\": "src/",
-            "MyApp\\Plugins\\": "plugins/"
-    }
+  "autoload": {
+    "psr-0": {
+      //new \PSR0\MyClass() -> ./psr-0/PSR0/MyClass.php
+      //new \PSR0\Another_MyClass() -> ./psr-0/PSR0/Another/MyClass.php (class Another_MyClass{})
+      "PSR0\\": "psr0/"
+    },
+    "psr-4": {
+      //new \PSR4\MyClass() -> ./psr-4/MyClass.php
+      "PSR4\\": "psr4/"
+    },
+    "classmap": [
+      //new \ClassMapNamespace\JustClass() -> ./classmap/MyClass.php
+      "classmap/",
+      //new \AnyNameSpace\JustClass() -> ./JustClass.php
+      "JustClass.php"
+    ],
+    "files": [
+      //Подгрузится файл functions.php с глобальными функциями 
+      "functions.php"
+    ]
+  }
 }
 ';
 
-//2. Создание автозагрузчика
+//2. Создание/обновление автозагрузчика
 //composer dump-autoload
 
 //3. Подключение автозагрузчика
-//require_once __DIR__ . 'vendor/autoload.php';
+//require_once __DIR__ . '/vendor/autoload.php';
+//или
+//require_once __DIR__ . '/../vendor/autoload.php';
 
 //4. Описание:
 // - psr-4 - регистр имеет значение
 // - \\ это экранированный \ в json
-// - Примеры классов:
-//    - MyLibrary\Some\Class -> src/Some/Class.php
-//    - MyApp\Plugins\Some\Class -> plugins/Some/Class.php
 // - Ссылки
 //    - https://getcomposer.org/doc/04-schema.md#psr-4
 
 
 
 
-//Dev-загрузчик
-'
-{
-    "require-dev": {
-    "phpunit/phpunit": "^5.5"
-    },
-    "autoload": {
-    "psr-4": {
-        "TDD\\": "src/"
-        }
-    },
-    "autoload-dev": {
-    "psr-4": {
-        "TDD\\Test\\": "tests/"
-        }
-    }
-}
-';
+
+//////////////////////////////////////////////////////////////////////////
+///////////////////////Автозагрузчик в коде///////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+
+
+//Подгрузка неймспейса в коде
+// instantiate the loader
+$loader = new \Example\Psr4AutoloaderClass;
+
+// register the autoloader
+$loader->register();
+
+// register the base directories for the namespace    prefix
+$loader->addNamespace('Foo\Bar', '/path/to/packages/foo-bar/src');
+$loader->addNamespace('Foo\Bar', '/path/to/packages/foo-bar/tests');
