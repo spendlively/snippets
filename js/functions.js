7e5eb89c4f39843666f9df98a6738b3d0636719d
Сизиков Константин <spendlively@mail.ru>
2018-08-28 13:17:47 +0700

    Функциональный стиль в js

--- js/functions.js
+++ js/functions.js
@@ -5,3 +5,36 @@ var arr = [1, 2, 3],
 
 arr.splice(arr.indexOf(el), 1);
 console.log(arr);
+
+
+
+
+
+//Очистка от нечисловых элементов в функциональном стиле
+function clean(elements){
+	return elements.filter((el) => !isNaN(Number(el)))
+}
+
+
+
+
+
+//Верификация нечисловых элементов в функциональном стиле
+function verify(elements){
+    elements.forEach((el) => {if(isNaN(Number(el))) throw new Error("Not a number")})
+}
+
+
+
+
+
+//Функция сложения чисел (с верификацией/очисткой) в функциональном стиле
+function sum() {
+    return Array.from(arguments).reduce((a, b) => Number(a) + Number(b), 0)
+}
+function sum() {
+    return verify(Array.from(arguments)).reduce((a, b) => Number(a) + Number(b), 0)
+}
+function sum() {
+    return clean(Array.from(arguments)).reduce((a, b) => Number(a) + Number(b), 0)
+}
