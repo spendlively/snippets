b4922f984be3f85470bcd34501ba864e8a7ae628
Konstantin Sizikov <spendlively@mail.ru>
2018-10-16 10:13:09 +0700

    makefile

---
+++ cpp/src/makefile/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "sum.h"
+
+using namespace std;
+
+int main() {
+
+    cout << "1 + 2 = " << sum(1, 2) << endl;
+
+    return 0;
+}
---
+++ cpp/src/makefile/sum.h
@@ -0,0 +1,6 @@
+#ifndef CPP_SUM_H
+#define CPP_SUM_H
+
+int sum(int a, int b);
+
+#endif //CPP_SUM_H
