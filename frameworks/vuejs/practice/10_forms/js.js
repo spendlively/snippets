96de01595f17656ef2036065ae1ee9e3f5dab9f6
spendlively <spendlively@mail.ru>
2019-06-18 22:20:54 +0700

    vue forms

---
+++ frameworks/vuejs/practice/10_forms/js.js
@@ -0,0 +1,91 @@
+
+
+new Vue({
+    el: '#e1',
+    data: {
+        message: 'qwerty',
+        checked: true
+    }
+})
+
+new Vue({
+    el: '#e2',
+    data: {
+        checkedNames: []
+    }
+})
+
+new Vue({
+    el: '#e3',
+    data: {
+        picked: 'Два'
+    }
+})
+
+new Vue({
+    el: '#e4',
+    data: {
+        selected: ''
+    }
+})
+
+new Vue({
+    el: '#e5',
+    data: {
+        selected: ''
+    }
+})
+
+new Vue({
+    el: '#e6',
+    data: {
+        selected: 'А',
+        options: [
+            { text: 'Один', value: 'А' },
+            { text: 'Два', value: 'Б' },
+            { text: 'Три', value: 'В' }
+        ]
+    }
+})
+
+new Vue({
+    el: '#e7',
+    data: {
+        picked: '',
+        toggle: '',
+        selected: ''
+
+    }
+})
+
+new Vue({
+    el: '#e8',
+    data: {
+        toggle: ''
+    }
+})
+
+new Vue({
+    el: '#e9',
+    data: {
+        pick: ''
+    }
+})
+
+new Vue({
+    el: '#e10',
+    data: {
+        selected: ''
+    }
+})
+
+new Vue({
+    el: '#e11',
+    data: {
+        msg: '',
+        age: '',
+        msg2: ''
+    }
+})
+
+
