fdef999caa463ab2d58fa1e140d61390ea979f1f
spendlively <spendlively@mail.ru>
2019-10-30 00:02:32 +0700

    struct

--- go/06_structures.go
+++ go/06_structures.go
@@ -8,8 +8,17 @@ type Vertex struct {
 	Y int
 }
 
+//метод и структура должны быть в одном пакете
+//здесь получатель Vertex используется по значению
+//т.е. sum работает с копией объекта
 func (v Vertex) sum () int {
     return v.X + v.Y
+}
+
+//здесь получатель Vertex используется по указателю
+//т.е. функция может изменять его данные
+func (v *Vertex) modifyX () {
+    v.X = v.X * 2
 }
 
 func main() {
@@ -24,5 +33,9 @@ func main() {
 	p := &v2
 	p.X = 1e9
 	fmt.Println("v2 =", v2)
+
+	v3 := Vertex{1, 2}
+	v3.modifyX()
+	fmt.Println(v3)
 }
 
