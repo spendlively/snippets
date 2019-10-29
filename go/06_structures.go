ae044c0882a37b11d9cbcca528c6476a3602a400
spendlively <spendlively@mail.ru>
2019-10-29 23:33:17 +0700

    structures with methods

--- go/06_structures.go
+++ go/06_structures.go
@@ -1,4 +1,4 @@
-//Struct
+
 package main
 
 import "fmt"
@@ -8,17 +8,21 @@ type Vertex struct {
 	Y int
 }
 
-func main() {
-	fmt.Println(Vertex{1, 2})
-	fmt.Println(Vertex.X)
-	fmt.Println(Vertex{X: 1})
-
-	v := Vertex{1, 2}
-	p := &v
-	p.X = 1e9
-	fmt.Println(v)
+func (v Vertex) sum () int {
+    return v.X + v.Y
 }
 
+func main() {
 
+    v1 := Vertex{1, 2}
+    fmt.Println("v1 =", v1)
+    fmt.Println("v1.sum() =", v1.sum())
+    fmt.Println("v1.X =", v1.X)
+    fmt.Println("Vertex{X: 1} =", Vertex{X: 1})
 
-//
+    v2 := Vertex{1, 2}
+	p := &v2
+	p.X = 1e9
+	fmt.Println("v2 =", v2)
+}
+
