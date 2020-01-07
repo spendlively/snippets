dafc972ebaba61c7b18901bd6cf068ad90273b25
spendmail <info@spendmail.ru>
2020-01-07 23:50:09 +0700

    python threads

--- python/practice/threads/simple.py
+++ python/practice/threads/simple.py
@@ -4,6 +4,7 @@ import threading
 import time
 
 
+# https://webdevblog.ru/vvedenie-v-potoki-v-python/
 def thread_function(name):
     logging.info("Thread %s: starting", name)
     time.sleep(30)
@@ -12,8 +13,7 @@ def thread_function(name):
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
-                        datefmt="%H:%M:%S")
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
     logging.info("Pid     : {0}".format(os.getpid()))
     logging.info("Main    : before creating thread")
@@ -23,5 +23,9 @@ if __name__ == "__main__":
     x.start()
 
     logging.info("Main    : wait for the thread to finish")
-    # x.join()
+
+    # Если не будет join, главный поток отработает до конца
+    # и потом все равно будет висеть в ожидании завершения дочернего
+    x.join()
+
     logging.info("Main    : all done")
