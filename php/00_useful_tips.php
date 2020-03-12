ade700656e9b6745a6abd3411f0f211d1f943ba2
spendmail <info@spendmail.ru>
2020-03-12 21:18:48 +0700

    new tips

--- php/00_useful_tips.php
+++ php/00_useful_tips.php
@@ -2,4 +2,5 @@
 
 //search files by extensions in path
 $files = glob("/path/to/directory/*.{jpg,gif,png}", GLOB_BRACE);
+//$files = glob("/mnt/media_remake/t/raw/{,*/,*/*/,*/*/*/,*/*/*/*/}*.psd", GLOB_BRACE);
 $filteredFiles = array_filter($files, 'is_file');
