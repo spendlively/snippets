c347deb0cfd2e4dc5dc330068b7b0a8802e498af
spendmail <info@spendmail.ru>
2021-01-22 12:47:15 +0700

    php aes

--- php/aes_symmetric_encryption.php
+++ php/aes_symmetric_encryption.php
@@ -16,7 +16,6 @@ $key = bin2hex($bytes);
 
 function encryptData($data, $key)
 {
-    $key = base64_decode($key);
     $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(ENCRYPTION_METHOD));
     $encrypted = openssl_encrypt($data, ENCRYPTION_METHOD, $key, 0, $iv, $tag);
     return $encrypted . ':' . base64_encode($iv) . ':' . base64_encode($tag);
@@ -24,7 +23,6 @@ function encryptData($data, $key)
 
 function decryptData($data, $key)
 {
-    $key = base64_decode($key);
     list($encryptedData, $iv, $tag) = explode(':', $data, 3);
     return openssl_decrypt($encryptedData, ENCRYPTION_METHOD, $key, 0, base64_decode($iv), base64_decode($tag));
 }
